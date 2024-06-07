3824b682190068e59cb63120c87fb206a7039d35
KasiaNejman <139686065+KasiaNejman@users.noreply.github.com>
2024-06-07 16:34:50 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React from 'react';
 
-import { Grid } from "react-loader-spinner";
+import { Grid } from 'react-loader-spinner';
 
 const Loader = () => {
   return (
@@ -8,7 +8,7 @@ const Loader = () => {
       <Grid
         height="80"
         width="80"
-        color="#3f51b5"
+        color="#8B008B"
         ariaLabel="grid-loading"
         radius="12.5"
         wrapperStyle={{}}
